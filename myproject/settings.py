2f1daf4ae8e1107f292909f6274fed0377b9ac35
khatiainanashvili <inanashvilikh@gmail.com>
2024-08-18 18:22:05 +0400

    sy

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-41p(c(e0$9#soyx!wyv8*n289tz_jw##hjb%w@4cnb6w81_m@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
